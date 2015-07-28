9f66b066df30ac04b0b9f5043779e6ecfccb36bf
Diego de Las Casas <dlsq@fireball.local>
2015-07-28 15:51:28 -0300

    Added exec-path-from-shell to fix Emacs.app

--- settings/window-sistem-settings.el
+++ settings/window-sistem-settings.el
@@ -6,6 +6,10 @@
 (text-scale-increase 1)
 
 (when (system-is-mac)
+  ;; Fix annoying problems with the exec-path in Emacs.app
+  (use-package exec-path-from-shell
+    :config (exec-path-from-shell-initialize)
+    :ensure t)
   ;; use command for meta (instead of option) in macs
   (setq mac-option-modifier 'super)
   (setq mac-command-modifier 'meta)
