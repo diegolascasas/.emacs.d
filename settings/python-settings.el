5ddaf92f91af4c38850b153e471c9e70618a244c
Diego de Las Casas <diegolascasas@gmail.com>
2015-12-04 14:23:41 -0200

    Changed OSX initialization and virtualenv settings

--- settings/python-settings.el
+++ settings/python-settings.el
@@ -1,7 +1,52 @@
 
+
+
+;; (use-package virtualenvwrapper
+;;   :commands venv-workon
+;;   :config
+;;   (venv-initialize-interactive-shells)
+;;   (venv-initialize-eshell)
+;;   (setq venv-location "~/.virtualenvs"))
+
+
+;; Hack to run postactivate and load its environment variables
+(require 'cl-lib)
+(defun venv-copy-envvars-postactivate ()
+  (let ((newvars
+	 (cl-remove-if-not
+	  (lambda (x) (and
+		       (string-match "=" x)
+		       (not (member x process-environment))
+		       (not (string-match "PATH=" x))))
+	  (split-string
+	   ;; run postactivate and copy its environment
+	   (shell-command-to-string (concat "source" " "
+					    (getenv "VIRTUAL_ENV")
+					    venv-executables-dir "/"
+					    "postactivate"
+					    "; "
+					    "env"
+					    ))
+	   "\n"))))
+    (if newvars
+	(dolist (envvar newvars)
+	  (setq process-environment
+		(cons envvar process-environment))))))
+
+
 (use-package virtualenvwrapper
-  :commands venv-workon
   :config
-  (venv-initialize-interactive-shells)
-  (venv-initialize-eshell)
-  (setq venv-location "~/.virtualenvs"))
+  (add-hook 'venv-postactivate-hook 'venv-copy-envvars-postactivate))
+
+;; (use-package pyvenv
+;;   :init
+;;   (exec-path-from-shell-copy-envs
+;;    (list
+;;     "WORKON_HOME"
+;;     "PROJECT_HOME"
+;;     "VIRTUALENVWRAPPER_PROJECT_FILENAME"
+;;     "VIRTUALENVWRAPPER_WORKON_CD"
+;;     "VIRTUALENVWRAPPER_SCRIPT"
+;;     "VIRTUALENVWRAPPER_HOOK_DIR"))
+;;   :commands pyvenv-workon
+;;   )
--- settings/window-sistem-settings.el
+++ settings/window-sistem-settings.el
@@ -5,9 +5,13 @@
 (text-scale-increase 1)
 
 (when (system-is-mac)
-  ;; Fix annoying problems with the exec-path in Emacs.app
-  (use-package exec-path-from-shell
-    :config (exec-path-from-shell-initialize))
+
+  ;; I'm calling emacs.app from a shell now, so this might not be needed
+  ;; Using this method: http://www.emacswiki.org/emacs/EmacsForMacOS#toc20
+  ;; ;; Fix annoying problems with the exec-path in Emacs.app
+  ;; (use-package exec-path-from-shell
+  ;;   :config (exec-path-from-shell-initialize))
+
   ;; use command for meta (instead of option) in macs
   (setq mac-option-modifier 'super)
   (setq mac-command-modifier 'meta)
