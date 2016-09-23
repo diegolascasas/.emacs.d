fed1e84664747d553cc37c5c61fe66a1e5e9e4f4
Diego <diego@Stellas-Mac-mini.local>
2016-09-23 18:05:44 -0300

    Fixed yasnippet X ansi-term interaction bug

--- settings/project-management-settings.el
+++ settings/project-management-settings.el
@@ -2,8 +2,12 @@
   :config (setq magit-last-seen-setup-instructions "1.4.0"))
 
 (use-package yasnippet
-  :config (yas-global-mode 1))
-
+  :config
+  (yas-global-mode 1)
+  ;; preserve tab-completion in ansi-term
+  (add-hook 'term-mode-hook (lambda()
+        (setq yas-dont-activate t)))
+  )
 ;; Manage projects
 (use-package projectile
   :config (projectile-global-mode))
